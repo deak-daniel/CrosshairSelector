a1d06f54245f7b9a98a3fe6875cf9b4db5acd71d
Deak Daniel <83765724+deak-daniel@users.noreply.github.com>
2024-09-26 01:03:53 +0200

    Added Key to letter mapping and vice versa.

--- CrosshairSelector/Core/Extensions/StringExtensions.cs
+++ CrosshairSelector/Core/Extensions/StringExtensions.cs
@@ -9,20 +9,99 @@ namespace CrosshairSelector
 {
     public static class StringExtensions
     {
+        private static readonly Dictionary<Key, string> KeyToLetterMap = new Dictionary<Key, string>() 
+        {
+            {Key.D0, "0" },
+            {Key.D1, "1"},
+            {Key.D2, "2"},
+            {Key.D3, "3"},
+            {Key.D4, "4"},
+            {Key.D5, "5"},
+            {Key.D6, "6"},
+            {Key.D7, "7"},
+            {Key.D8, "8"},
+            {Key.D9, "9"},
+            {Key.A, "a"},
+            {Key.B, "b"},
+            {Key.C, "c"},
+            {Key.D, "d"},
+            {Key.E, "e"},
+            {Key.F, "f"},
+            {Key.G, "g"},
+            {Key.H, "h"},
+            {Key.I, "i"},
+            {Key.J, "j"},
+            {Key.K, "k"},
+            {Key.L, "l"},
+            {Key.M, "m"},
+            {Key.N, "n"},
+            {Key.O, "o"},
+            {Key.P, "p"},
+            {Key.Q, "q"},
+            {Key.R, "r"},
+            {Key.S, "s"},
+            {Key.T, "t"},
+            {Key.U, "u"},
+            {Key.V, "v"},
+            {Key.W, "w"},
+            {Key.X, "x"},
+            {Key.Y, "y"},
+            {Key.Z, "z"},
+            {Key.Tab, "tab"},
+            {Key.LeftCtrl, "leftcontrol"},
+            {Key.LeftShift, "leftshift"},
+            {Key.Space, "space"}
+        };
+        private static readonly Dictionary<string, Key> LetterToKeyMap = new Dictionary<string, Key>()
+        {
+            {"0", Key.D0 },
+            {"1", Key.D1}, 
+            {"2", Key.D2},
+            {"3", Key.D3},
+            {"4", Key.D4},
+            {"5", Key.D5},
+            {"6", Key.D6},
+            {"7", Key.D7},
+            {"8", Key.D8},
+            {"9", Key.D9},
+            {"a", Key.A},
+            {"b", Key.B},
+            {"c", Key.C},
+            {"d", Key.D},
+            {"e", Key.E},
+            {"f", Key.F},
+            {"g", Key.G},
+            {"h", Key.H},
+            {"i", Key.I},
+            {"j", Key.J},
+            {"k", Key.K},
+            {"l", Key.L},
+            {"m", Key.M},
+            {"n", Key.N},
+            {"o", Key.O},
+            {"p", Key.P},
+            {"q", Key.Q},
+            {"r", Key.R},
+            {"s", Key.S},
+            {"t", Key.T},
+            {"u", Key.U},
+            {"v", Key.V},
+            {"w", Key.W},
+            {"x", Key.X},
+            {"y", Key.Y},
+            {"z", Key.Z},
+            {"space", Key.Space },
+            {"tab", Key.Tab },
+            {"leftshift", Key.LeftShift },
+            {"leftcontrol", Key.LeftCtrl }
+
+        };
         public static Key ToKey(this string value)
         {
-
             Key res = Key.None;
             if (value != null && value != "")
             {
-                if (value.Length > 0 && char.IsDigit(value[0]))
-                {
-                    res = (Key)(Convert.ToInt32(value) + (int)Key.D0);
-                }
-                if (res == Key.None)
-                {
-                    throw new FormatException("The string was either in a wrong format or was too long.");
-                }
+                res = LetterToKeyMap[value];
             }
             return res;
         }
